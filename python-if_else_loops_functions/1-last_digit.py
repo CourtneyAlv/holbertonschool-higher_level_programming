d2a781c965f7467f7dd0d41b52a90c8fc940edf7
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-06 20:04:11 -0700

    fixed pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10  '#Abs handles neg numbers'
+last_digit = abs(number) % 10  # Abs handles neg numbers
 
 message = f"Last digit of {number} is {last_digit} and is"
 
