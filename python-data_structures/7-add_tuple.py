efa64fa39e67213699899ef00d6a1cf8191a2019
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-10 21:19:57 -0700

    Write a function that adds 2 tuples.

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
 
-matrix = [[0] * 3 for i in range(3)]
+    element_1a = tuple_a[0] if len(tuple_a) > 0 else 0
+    element_2a = tuple_a[1] if len(tuple_a) > 1 else 0
+    element_1b = tuple_b[0] if len(tuple_b) > 0 else 0
+    element_2b = tuple_b[1] if len(tuple_b) > 1 else 0
 
-    for i in range(3):
-        for j in range(3):
-            matrix[i][j] = "{}, {},".format(i, j)
+    sum_1 = element_1a + element_1b
+    sum_2 = element_2a + element_2b
+
+    return sum_1, sum_2
