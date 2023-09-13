ac83a868611389127a938983d63a5aa1a668382a
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-12 20:04:10 -0700

    fixed syntax'

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,8 +2,6 @@
 
 def update_dictionary(a_dictionary, key, value):
 
-    new_dictionary = a_dictionary.copy()
 
-    new_dictionary[key] = value
-
-    return new_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
