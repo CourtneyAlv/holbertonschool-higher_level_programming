5c94cb1ddb14aca4242fe0311de0e72ee6689a80
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-17 00:05:27 -0700

    troubleshooting

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,11 +33,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        x, y = value
-        if not isinstance(x, int) or not isinstance(y, int) or x <= 0 or y <= 0:
-            raise TypeError('position must be a tuple of 2 positive integers')
+        if (not isinstance(value, tuple) or len(value) != 2
+            or not all(isinstance(num, int) for num in value)
+            or not all(num >= 0 for num in value)):
+          raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -47,9 +46,9 @@ class Square:
         # _ is a placeholder for a variable that you dont need in a loop
         if self.__size == 0:
             print("")
-        else:
-            x, y = self.__position
-            for _ in range(y):
-                print()
-            for _ in range(self.__size):
-                print('' * x + '#' * self.__size)
+            return
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
