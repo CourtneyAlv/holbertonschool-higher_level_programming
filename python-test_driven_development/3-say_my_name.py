2163721b263bd839570bc17c067a31a65da81174
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-18 22:27:39 -0700

    Write a function that prints My name is <first name> <last name>

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    """ A function that prints your first and last name
+        :first_name: (str) first argument
+        :last_name: (str) second argument
+        return: None"""
+
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+
+    if last_name:
+        full_name = first_name + " " + last_name
+    else:
+        full_name = first_name
+    print("My name is", full_name)
