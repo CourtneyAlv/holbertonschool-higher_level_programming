75b14f6c4118a0dea8d8ce789e312ec147b419d5
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-18 21:25:05 -0700

    fixed pycode style

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,7 @@ def matrix_divided(matrix, div):
 
     if not isinstance(matrix, list) or not all(
             all(isinstance(e, (int, float)) for e in row) for row in matrix):
-        raise TypeError('matrix must be a matrix (list of lists)'
+        raise TypeError('matrix must be a matrix (list of lists) '
                         'of integers/floats')
 
     row_lengths = set(len(row) for row in matrix)
