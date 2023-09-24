6ea4b178950ef399be810da0548de33088b5a1d7
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-23 22:05:49 -0700

    testing my file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a class MyList that inherits from list"""
+
+
+class Mylist(list):
+    """Represents a list"""
+    def print_sorted(self):
+        """ prints a list, but sorted (ascending sort)
+                args:
+                self (Mylist): The Mylist instance to be sorted and printed
+                    """
+        print(sorted(self))
