43cd3f89060a286312d62161cd63cbcd10f14ec9
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-23 23:58:45 -0700

    added more docstrings file 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,9 +5,21 @@
 class BaseGeometry:
     """Defines an empty class"""
     def area(self):
+        """ Computes the area of the geometry
+            The method is not implemented in the base class
+            Raises
+                Exception: raise exception if are() id not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validate an integer value
+            Args:
+                name (str): whats being validated
+                value (int): the value to be validated
+            Raises:
+                TypeError: if not an int
+                ValueError: if less than or equal to 0
+                """
 
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
