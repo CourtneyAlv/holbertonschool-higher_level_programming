4d07125a9afd88db2f80549e3a4e4e0f9caf40dd
CourtneyAlv <voiceoftruth42@gmail.com>
2023-09-24 20:41:50 -0700

    testing file 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 """ function that returns the dictionary description with simple data structure
 """
-import json
 
 
 def class_to_json(obj):
-    """converts an object to a dictionary for json
-        Args:
-            obj: instance of a class
-        Returns:
-            dict: dictionary rep
-        """
+    """converts an object to a dictionary for json"""
+
     return obj.__dict__
