05803c875439861a428d24bdadc0e0c710f61006
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-01 18:30:13 -0700

    touched up docstring

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,10 +8,16 @@ class Rectangle(Base):
     """ Represents a rectangle/ inherited from class Base """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-
+        """Class constructor
+            Args:
+                width (int): the width of the rectangle
+                height (int): the height of the rectangle
+                x (int): the x coordinate of the rectangle
+                y (int): the y coordinate of the rectange
+                id (int): intger representing id
+            """
         self.__width = width    # private attrubutes ↆ
         self.__height = height
         self.__x = x
         self.__y = y
-
         super().__init__(id)
