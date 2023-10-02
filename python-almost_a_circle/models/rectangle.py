02bbf794190d5cded6c9711871f427e27393c167
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-01 20:42:18 -0700

    improved my display method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,8 +77,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints in stdout the rect. instance with char '#' """
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Returns/overrides the string rep of Rectangle"""
