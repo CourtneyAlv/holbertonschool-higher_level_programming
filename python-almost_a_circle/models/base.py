160551755adb16abfca72f48972d17ad247c4bf6
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-02 21:21:02 -0700

    returns the JSON string rep. of list_dictionaries

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """Define a base class model"""
+import json
+
 
 class Base:
     """ Represent a Base Class"""
@@ -17,3 +19,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ that returns the JSON string rep. of list_dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
