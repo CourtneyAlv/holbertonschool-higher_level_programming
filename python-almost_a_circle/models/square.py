dab95d000e628f6a3daa3575d0f8708a11ecbad5
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-02 20:30:03 -0700

    added public getter and setter for square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,3 +19,12 @@ class Square(Rectangle):
     def __str__(self):
         """overloads __str__ method"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
