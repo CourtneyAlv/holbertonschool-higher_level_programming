6108f3e2e72a069ebd47d6a09d1f4fcdc4f47605
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-23 12:37:05 -0700

    fixed syntax and pycodstyle on file 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.  """
+""" script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument."""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -17,14 +16,15 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     state_name = sys.argv[4]
-    
-    query = ("SELECT * FROM states WHERE = '{}' ORDER BY id ASC".format(state_name)
+
+    query = "SELECT * FROM states WHERE name = '{}' " \
+            "ORDER BY id ASC".format(state_name)
     cursor.execute(query)
 
     result_states = cursor.fetchall()
 
     for state in result_states:
-            print(state)
+        print(state)
 
     cursor.close()
     db.close()
