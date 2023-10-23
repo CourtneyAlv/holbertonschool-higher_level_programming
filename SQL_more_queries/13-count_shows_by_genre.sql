1dfd5829a5f3d1dbe8a8b3794b20e211ac8fa8f8
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-22 21:05:28 -0700

    working on syntax for file 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_show_genres.genre AS genre, COUNT(tv_shows.title) AS number_of_shows
-FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_shows_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
-HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
