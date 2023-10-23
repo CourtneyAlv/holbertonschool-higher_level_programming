0aa68fd3911aad40e9b8b0e21232b8e67e47ae60
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-23 16:44:29 -0500

    pushing to terminal model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 """python file that contains the class definition of a State and an
 instance Base = declarative_base() """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
