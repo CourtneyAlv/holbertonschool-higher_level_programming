b18b327e930b80ce44cc82e9214755a880b7f6c7
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-23 10:51:14 -0500

    im not sure 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-""" Lists all the states from mySQL database """
+""" Write a script that lists all states from the database hbtn_0e_0_usa:
+Your script should take 3 arguments: mysql username, mysql password and database name 
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported """
 import sys
 import MySQLdb
 
-
-def list_states(username, password, database):
-    """
-        lists all states from the database hbtn_0e_0_usa
-        ARGS:
-            username: MYSQL Username
-            password: MYSQL password
-            dtatbase: dtatabase name
-    """
-
-
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database
-)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
 cursor = db.cursor()
 
@@ -33,10 +29,3 @@ for list_st in list_sts:
 
 cursor.close()
 db.close()
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-list_states(username, password, database)
