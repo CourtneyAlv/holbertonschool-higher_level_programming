dace1e33865e2bd2fc042807bfde32c19d85320f
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-23 14:41:46 -0700

    pushing so i can work on github

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
-"""python file that contains the class definition of a State and an 
+!/usr/bin/python3
+"""python file that contains the class definition of a State and an
 instance Base = declarative_base() """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,11 +10,12 @@ Base = declarative_base()
 
 
 class State(Base):
-  """Representing a state for MySQL dtatabase.
-    __tablename__: (str): name of MySQL table to store states
+    """Representing a state for MySQL dtatabase.
+
+     __tablename__: (str): name of MySQL table to store states
     id int: State Id
     name str: state name
-  """
-  __tablename__ = 'states'
-  id = Column(Integer, primary_key=True)
-  name = Column(String(128), nullable=False)
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
