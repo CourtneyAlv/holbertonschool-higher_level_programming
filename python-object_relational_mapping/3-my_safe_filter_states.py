f17a534924c3d19df82a05b68fccd9f2af2cd4c6
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-23 14:42:09 -0500

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+""" write a script that is safe from MySQL injections! """
+import sys
+import MySQLdb
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    state_name = sys.argv[4]
+
+    query = "SELECT * FROM states WHERE name = %s " \
+            "ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    result_states = cursor.fetchall()
+
+    for state in result_states:
+        print(state)
+
+    cursor.close()
+    db.close()
