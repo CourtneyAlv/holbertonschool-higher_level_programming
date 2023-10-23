6424dcdda4d67493f6efefee9c4191bfae5d40f7
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-23 16:05:41 -0500

    original 1 attempting to test code 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+""" write a script that is safe from MySQL injections! """
+import sys
+import MySQLdb
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+  stat_name = sys.argv[4]
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM cities WHERE state_id IN " /
+        "(SELECT id FROM states WHERE name = %s) ORDER BY cities.id ASC"
+    cursor.execute(query, (state_name,))
+
+    result_cities = cursor.fetchall()
+
+    for city in result_cities:
+        print(cities)
+
+    cursor.close()
+    db.close()
