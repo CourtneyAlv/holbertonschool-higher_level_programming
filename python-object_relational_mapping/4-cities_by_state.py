bdd8ff9b7a84851d95f4806050391f6d3f94005f
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-23 15:32:32 -0500

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+""" write a script that is safe from MySQL injections! """
+import sys
+import MySQLdb
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    state_name = sys.argv[4]
+
+    query = "SELECT * FROM cities " \
+            "ORDER BY id ASC"
+    cursor.execute(query)
+
+    result_cities = cursor.fetchall()
+
+    for city in result_cities:
+        print(city)
+
+    cursor.close()
+    db.close()
