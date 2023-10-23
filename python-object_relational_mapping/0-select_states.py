f9d02652b57d69cd3bfbe705244fc14bbe03dd6a
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-23 10:20:49 -0500

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,20 @@
 import sys
 import MYSQLdb
 
-def list_states(username, password, database):
-
+def list_states(username, password, database_name):
+    """ 
+        lists all states from the database hbtn_0e_0_usa
+        ARGS:
+            username: MYSQL Username
+            password: MYSQL password
+            dtatbase: dtatabase name
+    """
+    
 db = MYSQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
 
 cursor = db.cursor()
 
-cursor.execute("Select * FROM states ORDER BY id ASC")
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
 states = cursor.fetchall()
 
@@ -20,4 +27,8 @@ cursor.close()
 db.close()
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
+list_states(username, password, database)
