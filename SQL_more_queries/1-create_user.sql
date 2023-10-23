e146715679870f22bbff600ebde4eeb37d905dc8
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-22 19:28:19 -0700

    fixed my syntax

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- creates the MySQL server user user_0d_1.
-CREATE OR REPLACE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_Od_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_Od_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
