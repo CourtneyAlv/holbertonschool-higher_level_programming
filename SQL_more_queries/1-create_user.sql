fdedae4b241d7930fe927496a44becca98bc2368
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-22 19:35:21 -0700

    fixed a typo

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_Od_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES on * . * to user_0d_1@localhost;
