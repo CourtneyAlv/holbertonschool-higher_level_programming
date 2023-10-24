5fbacbcb071ec4c0f1b73d4625f1634b6c55dd0c
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-24 15:35:55 -0500

    fixed many typos 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that lists all State objects from the database hbtn_0e_6_usa """
-from sys import argv 
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     session = Session()
 
     for inst in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+        print(inst.id, inst.name, sep=": ")
