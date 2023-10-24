bf74e7777e734518b7cc295ec903ac52488fa459
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-24 14:19:56 -0500

    workied on syntax 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,11 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM cities ORDER BY id ASC"
+    query = """SELECT cities.names, cities.id, states.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC"""
+
     cursor.execute(query)
 
     result_cities = cursor.fetchall()
