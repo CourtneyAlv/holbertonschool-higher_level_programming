5d0483378b895f0a18309297b0bdcd58fda6799c
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-24 16:35:35 -0500

    modified code, testing checker 11-model_state_insert.py

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter_by(name="Louisiana').first()
+    new_instance = session.query(State).filter_by(name="Louisiana').first()
     print(new_instance.id)
     session.commit()
