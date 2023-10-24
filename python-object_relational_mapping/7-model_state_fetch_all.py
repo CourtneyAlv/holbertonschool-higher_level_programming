138a33c3cdeff920eb79a902c5cd772cb369c9a7
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-24 15:42:27 -0500

    Update 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that lists all State objects from the database hbtn_0e_6_usa """
-from sys import argv
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
