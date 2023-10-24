ae9634eea6c752196d5e9db5d8b97c2f5d5e3bef
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-24 16:29:20 -0500

    working on pycode10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     instance = session.query(State).filter(State.name == (sys.argv[4],))
+
     if instance
         print(instance[0].id)
     else:
