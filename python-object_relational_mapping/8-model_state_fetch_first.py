ddba1ab30ee082bc3b1b5d4ff29edc1d6b790fb1
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-24 13:54:51 -0700

    fixed pycode

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that prints the first State object from the database hbtn_0e_6_usa """
+"""script that prints the first State object from database hbtn_0e_6_usa"""
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
