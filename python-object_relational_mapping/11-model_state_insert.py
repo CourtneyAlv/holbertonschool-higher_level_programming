86b8f409ea924572b5ca4b0b682384fad7f0a9fa
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-24 19:34:16 -0700

    need to learn how to spell louisiana

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that adds the State object “Louisiana” 
+"""script that adds the State object “Louisiana”
 to the database hbtn_0e_6_usa"""
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name='Lousiana')
+    new_state = State(name='Louisiana')
     session.add(new_state)
     new_inst = session.query(State).filter_by(name='Louisiana').first()
     print(new_inst.id)
--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""deletes all State objects with a name
-containing the letter a from the database hbtn_0e_6_usa"""
+""" prints all City objects from the database hbtn_0e_14_usa"""
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
