21e4c7f42cdb4fa8c139fe3031525d586edd8af9
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-24 19:52:33 -0700

    worked on model_city file

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+""" Python file similar to model_state.py named model_city.py that
+contains the class definition of a City."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
+Base = declarative_base()
+
+
+class City(Base):
+    """defines each city
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"). nullable=False)
