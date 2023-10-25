5b7f8fe63b882358734c37dbbbd08fb970f55f15
CourtneyAlv <voiceoftruth42@gmail.com>
2023-10-24 19:53:57 -0700

    working in syntax

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,8 +5,6 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
-Base = declarative_base()
-
 
 class City(Base):
     """defines each city
